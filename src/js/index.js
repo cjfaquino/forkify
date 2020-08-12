df9d07ab458a58775798bc717be71b51d6ae6ced
Christian Aquino <cjfaquino@gmail.com>
2020-08-11 21:16:37 -0700

    add search function

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,38 @@
-// Global app controller
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from './views/base';
 
-import str from './models/Search';
-import { add, multiply, ID } from './views/searchView';
+/*Global state of app
+    - Search object
+    - Current recipe obj
+    - Shopping list obj
+    - Linked recipes */
+const state = {};
 
-console.log(`using imported functions ${add(ID, 2)} and ${multiply(3, 5)}. ${str}`);
+const controlSearch = async () => {
+  //1 get query from view
+  const query = searchView.getInput();
+  console.log(query);
+
+  if (query) {
+    //2 new search obj and add to state
+    state.search = new Search(query);
+
+    //3 prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
+    renderLoader(elements.searchRes);
+
+    //4 search for recipes
+    await state.search.getResults();
+
+    //5 render results in ui
+    clearLoader();
+    searchView.renderResults(state.search.result);
+  }
+};
+
+elements.searchForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  controlSearch();
+});
