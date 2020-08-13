b04c1c2a7a0671b39a72f14e46e08d8cb1ed0975
Christian Aquino <cjfaquino@gmail.com>
2020-08-12 21:29:59 -0700

    add shopping list

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /*Global state of app
@@ -10,6 +12,7 @@ import { elements, renderLoader, clearLoader } from './views/base';
     - Shopping list obj
     - Linked recipes */
 const state = {};
+window.state = state;
 
 //SEARCH CONTROLLER
 
@@ -91,6 +94,37 @@ const controlRecipe = async () => {
 
 ['hashchange', 'load'].forEach((event) => window.addEventListener(event, controlRecipe));
 
+//LIST CONTROLLER
+const controlList = () => {
+  //create new list if theres none
+  if (!state.list) state.list = new List();
+
+  //add each ing to list and UI
+  state.recipe.ingredients.forEach((el) => {
+    const item = state.list.addItem(el.count, el.unit, el.ingredient);
+    listView.renderItem(item);
+  });
+};
+
+//handle delete and  update list item evens
+elements.shopping.addEventListener('click', (e) => {
+  const id = e.target.closest('.shopping__item').dataset.itemid;
+
+  //handle delete button
+  if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+    //delete from state
+    state.list.deleteItem(id);
+
+    //delete from UI
+    listView.deleteItem(id);
+
+    //handle count update
+  } else if (e.target.matches('.shopping__count-value')) {
+    const val = parseFloat(e.target.value, 10);
+    state.list.updateCount(id, val);
+  }
+});
+
 //handling recipe button clicks
 elements.recipe.addEventListener('click', (e) => {
   if (e.target.matches('.btn-decrease, .btn-decrease *')) {
@@ -102,5 +136,9 @@ elements.recipe.addEventListener('click', (e) => {
   if (e.target.matches('.btn-increase, .btn-increase *')) {
     state.recipe.updateServings('inc');
     recipeView.updateServingsIngredients(state.recipe);
+  } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+    controlList();
   }
 });
+
+window.l = new List();
